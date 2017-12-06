8449714f8fc0bfad82245594d9abc3e37b20111a
Matt <mattxtsao@gmail.com>
2017-12-06 13:53:34 -0800

    naive fullstate

--- policies.py
+++ policies.py
@@ -2,61 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import utils
-
-def Q_Learning(T,R):
-    # ReadMe
-        # functionality
-            # performs Q-learning to train the attacker versus a static defender policy.
-            # this function is only designed to work for N = 2, A = 5 (num_players, num_actions respectively)
-        # INPUTS
-            # The training data should have the sequential form (s,a,r,s'), similar to project 2.
-        # OUTPUTS
-            # pol is the optimal policy for the learned Q values.
-            # Q is the matrix of size num_states by num_actions specifying the quality of a particular state action pair.
-
-    L = n*m
-    N = 2
-    A = 5
-
-    num_iters = 5
-
-    Q = np.zeros((L**N, A**N))
-
-    for t in range(num_iters): # do this num_iters times
-        print "Q Learning iteration %s/%s" % (t,num_iters)
-        for s in range(L**N):
-            for a_a in range(A): # loop over the attacker's action
-                d_a = Naive_D(s,n,m,1, a_a, 1) # determine the defender's action from Naive_D
-                acts = np.zeros((2,))
-                acts[0] = a_a
-                acts[1] = d_a
-                a = int(utils.act2vec(acts, A)) # get the full action
-
-                temp = T[s,a,:,:] # find the transition information from T
-                k = temp.shape[0]
-                new_Q_val = R[s,a] # include the immediate reward
-
-                for i in range(k): # for each possible next state s', we need to find max_{a'} Q(s',a')
-                    sp = int(temp[i,0]) # this is one possible next state
-
-                    max_Q = -1
-
-                    for next_a_a in range(A): # loop over a' to find max_{a'} Q(s',a')
-                        next_d_a = Naive_D(sp, n, m, 1, next_a_a, 1)
-                        next_acts = np.zeros((2,))
-                        next_acts[0] = next_a_a
-                        next_acts[1] = next_d_a
-                        next_a = int(utils.act2vec(next_acts, A)) # get the full next action
-
-                        if (Q[sp, next_a] > max_Q):
-                            max_Q = Q[sp, next_a]
-
-                    new_Q_val = new_Q_val + temp[i,1]*max_Q
-
-                Q[s,a] = new_Q_val
-
-    return Q
+from utils import *
 
 def Naive_D(s, n, m, lookahead, a, p):
     # ReadMe
@@ -94,10 +40,10 @@ def Naive_D(s, n, m, lookahead, a, p):
         mov[0] = a_obs
         mov[1] = 0
 
-        pre_pos = utils.state2pos(s, n, m, 2) # get the current position
-        pos, bounds = utils.mov2pos(mov, pre_pos, n,m) # generate the new position. The attacker is player 1, the defender is player 2
+        pre_pos = state2pos(s, n, m, 2) # get the current position
+        pos, bounds = mov2pos(mov, pre_pos, n,m) # generate the new position. The attacker is player 1, the defender is player 2
     else: # if we do not allow lookahead,
-        pos = utils.state2pos(s, n, m, 2)
+        pos = state2pos(s, n, m, 2)
 
     # print pos
 
@@ -116,18 +62,89 @@ def Naive_D(s, n, m, lookahead, a, p):
         else:
             return 2 # in this case, the attacker will try to advance, so you should follow him.
 
+def Naive_Fullstate(m,n,p):
+    L = n*m
+    N = 2
+    A = 5
 
+    full_a = np.zeros((L**N, A))
+
+    for s in range(L**N):
+        for a_a in range(A):
+            d_a = Naive_D(s,n,m,1,a_a, p)
+            acts = np.zeros((2,))
+            acts[0] = a_a
+            acts[1] = d_a
+            full_a[s,a_a] = act2vec(acts,A)
+
+    return full_a
 
 if __name__ == '__main__':
     # just some test code to see if the Naive_D function is working properly.
-    n = 10
-    m = 10
-    pos = np.zeros((2,2))
-    pos[0,0] =  5 # x1
-    pos[0,1] =  5 # y1
-    pos[1,0] =  5 # x2
-    pos[1,1] =  6 # y2
+    # n = 10
+    # m = 10
+    # pos = np.zeros((2,2))
+    # pos[0,0] =  5 # x1
+    # pos[0,1] =  5 # y1
+    # pos[1,0] =  5 # x2
+    # pos[1,1] =  6 # y2
+    #
+    # s = utils.pos2state(pos,n,m,2)
+    #
+    # print Naive_D(s, n, m, 1, 3, 1)
 
-    s = utils.pos2state(pos,n,m,2)
+    a = Naive_Fullstate(5,5,1)
 
-    print Naive_D(s, n, m, 1, 3, 1)
+# def Q_Learning(T,R):
+#     # ReadMe
+#         # functionality
+#             # performs Q-learning to train the attacker versus a static defender policy.
+#             # this function is only designed to work for N = 2, A = 5 (num_players, num_actions respectively)
+#         # INPUTS
+#             # The training data should have the sequential form (s,a,r,s'), similar to project 2.
+#         # OUTPUTS
+#             # pol is the optimal policy for the learned Q values.
+#             # Q is the matrix of size num_states by num_actions specifying the quality of a particular state action pair.
+#
+#     L = n*m
+#     N = 2
+#     A = 5
+#
+#     num_iters = 5
+#
+#     Q = np.zeros((L**N, A**N))
+#
+#     for t in range(num_iters): # do this num_iters times
+#         print "Q Learning iteration %s/%s" % (t,num_iters)
+#         for s in range(L**N):
+#             for a_a in range(A): # loop over the attacker's action
+#                 d_a = Naive_D(s,n,m,1, a_a, 1) # determine the defender's action from Naive_D
+#                 acts = np.zeros((2,))
+#                 acts[0] = a_a
+#                 acts[1] = d_a
+#                 a = int(utils.act2vec(acts, A)) # get the full action
+#
+#                 temp = T[s,a,:,:] # find the transition information from T
+#                 k = temp.shape[0]
+#                 new_Q_val = R[s,a] # include the immediate reward
+#
+#                 for i in range(k): # for each possible next state s', we need to find max_{a'} Q(s',a')
+#                     sp = int(temp[i,0]) # this is one possible next state
+#
+#                     max_Q = -1
+#
+#                     for next_a_a in range(A): # loop over a' to find max_{a'} Q(s',a')
+#                         next_d_a = Naive_D(sp, n, m, 1, next_a_a, 1)
+#                         next_acts = np.zeros((2,))
+#                         next_acts[0] = next_a_a
+#                         next_acts[1] = next_d_a
+#                         next_a = int(utils.act2vec(next_acts, A)) # get the full next action
+#
+#                         if (Q[sp, next_a] > max_Q):
+#                             max_Q = Q[sp, next_a]
+#
+#                     new_Q_val = new_Q_val + temp[i,1]*max_Q
+#
+#                 Q[s,a] = new_Q_val
+#
+#     return Q
